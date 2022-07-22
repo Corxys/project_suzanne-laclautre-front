1a54b9abf271f074d0f6962a63ae41963e417561
Corxys <jennblngr@gmail.com>
2022-07-22 17:01:53 +0200

    :briefcase: installation of dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,16 @@
 export default {
+  srcDir: 'src',
+  buildDir: 'dist',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'suzanne-laclautre',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'fr'
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
@@ -18,6 +21,9 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    // See: https://nicolas-cusan.github.io/destyle.css/
+    'node_modules/destyle.css/destyle.min.css',
+    '~/assets/styles/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -27,11 +33,22 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  styleResources: {
+    scss: [
+      '~/assets/styles/variables.scss',
+      '~/assets/styles/mixins.scss'
+    ]
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    // See: https://www.npmjs.com/package/@nuxtjs/style-resources
+    '@nuxtjs/style-resources'
   ],
+
+  // googleFonts: ,
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
