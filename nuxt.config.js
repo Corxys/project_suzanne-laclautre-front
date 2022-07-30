d5a74bb34d705f31465f8888d835c446af76236a
Corxys <jennblngr@gmail.com>
2022-07-30 14:39:32 +0200

    :wrench: added Google Fonts API in the HTML head

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'suzanne-laclautre',
+    title: 'Suzanne Laclautre',
     htmlAttrs: {
       lang: 'fr'
     },
@@ -16,7 +16,11 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Eczar:wght@400;700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+      }
     ]
   },
 
